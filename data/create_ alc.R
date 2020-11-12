b2e164e951b17b64f4120182204c31f82b5a1f2b
Palsola <minttu@ad.helsinki.fi>
2020-11-12 15:43:07 +0200

    Did the data wrangling exercise

---
+++ data/create_ alc.R
@@ -0,0 +1,7 @@
+# Minttu Palsola, 12.11.2020
+# This data approach student achievement in secondary education of two Portuguese schools. The data attributes include student grades, demographic, social and school related features) and it was collected by using school reports and questionnaires. 
+# Source: Paulo Cortez, University of Minho, GuimarÃ£es, Portugal, http://www3.dsi.uminho.pt/pcortez
+# Data available at: https://archive.ics.uci.edu/ml/datasets/Student+Performance
+
+math <- read.csv("C:/Users/minttu/R projects/IODS-project/data/student-mat.csv", header = T)
+por <- read.csv("C:/Users/minttu/R projects/IODS-project/data/student-por.csv", header = T)

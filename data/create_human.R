7f873e3e76cd829393faf586f8f6adbf9a37238c
Palsola <minttu@ad.helsinki.fi>
2020-11-18 22:24:19 +0200

    Data wrangling

---
+++ data/create_human.R
@@ -0,0 +1,4 @@
+Read the “Human development” and “Gender inequality” datas into R
+
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
